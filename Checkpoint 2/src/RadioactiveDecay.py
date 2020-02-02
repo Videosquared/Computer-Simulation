09c6b117dd4ae0b8f28ebe2198c52767faea9197
Yeyao L <yliu.personal@gmail.com>
2020-02-02 20:05:28 +0000

    Started CP2

---
+++ Checkpoint 2/src/RadioactiveDecay.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+class RadDecay(object):
+  pass
+
+#END CLASS RadDecay
---
+++ Checkpoint 2/src/TestingRAD.py
@@ -0,0 +1,10 @@
+from RadioactiveDecay import *
+
+def main():
+  print("HELLLLLLLLLLOOOOOOOOOOOO")
+#END main()
+
+
+#This is used to make the program run automatically without any arguments 
+if __name__ == "__main__":
+  main()
