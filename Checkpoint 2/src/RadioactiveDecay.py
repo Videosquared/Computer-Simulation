6d60e3a78854ff399164085afa26c0fe20c10f37
Yeyao L <yliu.personal@gmail.com>
2020-02-04 21:14:59 +0000

    inf looping

--- Checkpoint 2/src/RadioactiveDecay.py
+++ Checkpoint 2/src/RadioactiveDecay.py
@@ -1,6 +1,68 @@
 import numpy as np
+import random 
 
 class RadDecay(object):
-  pass
 
-#END CLASS RadDecay
+  # This is used to setup the array to the right size. 
+  def __init__(self, decay_Const, array_Size, time_Stamp):
+    self.arraySize = array_Size
+    self.decayConst = decay_Const
+    self.timeStamp = time_Stamp
+  #END __init__()
+
+
+  # This will display all the iterations of the decay and then
+  # display the final half life when the flag is set to true
+  def display(self):
+    pass
+    """while True:
+      if flag == True:
+        print(time)
+        print(initail nuclei)
+        print(final nuclei)
+        print(simulated half-life)
+        print(actual halflife )
+        break"""
+  #END display()
+  
+  # This starts an infinite loop to calculate the half life of the
+  # element and then return the time
+  def calculate(self):
+    #flag = False 
+    num_decayed = 0 
+    out_array = np.zeros((self.arraySize,self.arraySize))
+    radTime = 0.00
+
+    while True:
+      if num_decayed == ((self.arraySize * self.arraySize) / 2):
+        break
+      else:
+        for i in range(self.arraySize):
+          for j in range(self.arraySize):
+            if out_array[i,j] == 0:
+              rnd_num = random.random()
+              probability = (self.decayConst * radTime)
+              if probability <= rnd_num:
+                out_array[i,j] = 1
+                num_decayed += 1
+              #end if 
+            #end if 
+            radTime += self.timeStamp
+          #end for 
+        #end for
+      #end if 
+    #end while
+    return out_array
+  #END calculate()
+
+  # This is used to calculate the actual half life of the element
+  # using the formula and return the value. 
+  def calculateActual(self):
+    pass
+  #END calculateActual()
+
+
+
+    
+
+#END CLASS 
--- Checkpoint 2/src/TestingRAD.py
+++ Checkpoint 2/src/TestingRAD.py
@@ -1,7 +1,35 @@
 from RadioactiveDecay import *
 
+import os
+import sys
+import numpy as np
+
+# This will ask the user to input the values required and then call the class to start
+# the simulation. 
 def main():
-  print("HELLLLLLLLLLOOOOOOOOOOOO")
+
+  decay_Const = float(input("Please enter the value for the decay Constant (λ): "))
+  array_Size = int(input("Please enter the size of the array (N): "))
+  time_Stamp = float(input("Please enther the timestamp (Δt): "))
+
+  rad1 = RadDecay(decay_Const, array_Size, time_Stamp)
+  rad2 = RadDecay.calculate(rad1)
+  #rad3 = RadDecay.calculateActual(rad1)
+  #RadDecay.display(rad1, rad2, rad3)
+
+  np.set_printoptions(threshold=np.inf, linewidth = np.inf)
+  print(rad2)
+  
+  
+"""
+
+test = np.zeros((array_Size, array_Size))
+
+print(test)
+test[0,0] = 1
+"""
+  
+
 #END main()
 
 
