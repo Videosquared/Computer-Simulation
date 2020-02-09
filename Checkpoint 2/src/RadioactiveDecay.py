604b4ad71f4ebc1e1d768daba59828d9cb2f12e7
Yeyao L <yliu.personal@gmail.com>
2020-02-09 16:09:10 +0000

    Polish code with Polish guy

--- Checkpoint 2/src/RadioactiveDecay.py
+++ Checkpoint 2/src/RadioactiveDecay.py
@@ -3,54 +3,48 @@ import random
 
 class RadDecay(object):
 
-  # This is used to setup the array to the right size. 
-  def __init__(self, decay_Const, array_Size, time_Stamp):
-    self.arraySize = array_Size
-    self.decayConst = decay_Const
-    self.timeStamp = time_Stamp
-  #END __init__()
+    # This is used to hold the values we need about this element  
+    def __init__(self, decay_Const, array_Size, time_Stamp):
+        self.arraySize = array_Size
+        self.decayConst = decay_Const
+        self.timeStamp = time_Stamp
+     #END __init__()
   
-  # This starts an infinite loop to calculate the half life of the
-  # element and then display it in an array form.
-  def calculateDecay(self): 
-    num_decayed = 0 
-    out_array = np.zeros((self.arraySize,self.arraySize))
-    radTime = 0.0
+    # This starts an loop to calculate the half life of the
+    # element and then display it in an array form, as well as calculate the
+    # simulated decay time
+    def calculateDecay(self): 
+        num_decayed = 0 
+        out_array = np.zeros((self.arraySize,self.arraySize), dtype=int) #creating an numpy array of ints, to remove the . after each number
+        radTime = 0.0
 
-    while True:
-      if num_decayed >= ((self.arraySize * self.arraySize) / 2):
-        break
-      else:
-        for i in range(self.arraySize):
-          for j in range(self.arraySize):
-            if out_array[i,j] == 0:
-              rnd_num = (random.random())
-              probability = (self.decayConst * self.timeStamp)
-              if rnd_num < probability:
-                out_array[i,j] = 1
-                num_decayed += 1
+        while num_decayed < ((self.arraySize * self.arraySize) / 2): 
+            for i in range(self.arraySize):
+                for j in range(self.arraySize):
+                    if out_array[i,j] == 0:
+                        rnd_num = random.random()
+                        probability = self.decayConst * self.timeStamp
+                        if rnd_num <= probability:
+                            out_array[i,j] = 1
+                            num_decayed += 1
 
-      radTime += self.timeStamp
+            radTime += self.timeStamp
   
-    np.set_printoptions(threshold=np.inf, linewidth = np.inf)
-    print(out_array)
-    print("\n")
-    print("The simulated half life is: ", round(radTime, 2), " mins")
-    print("Number of nuclei decayed: ", num_decayed)
-    print("The original number of nuclei: ", (self.arraySize * self.arraySize))
-  #END calculate()
+        np.set_printoptions(threshold=np.inf, linewidth = np.inf)
+        print(out_array)
+        print("\n")
+        print("The simulated half life is: ", round(radTime, 2), " mins")
+        print("Number of nuclei decayed: ", num_decayed)
+        print("The original number of nuclei: ", (self.arraySize * self.arraySize))
+    #END calculate()
 
-  # This is used to calculate the actual half life of the element
-  # using the formula and display the value. 
-  def calculateActual(self):
-    actual_time = np.log(2)/self.decayConst
-    actual_time = round(actual_time, 2)
+    # This is used to calculate the actual half life of the element
+    # using the formula and display the value. 
+    def calculateActual(self):
+        actual_time = np.log(2)/self.decayConst
+        actual_time = round(actual_time, 2)
 
-    print("The actual value of half life (using decay consant): ", actual_time , " mins")
-  #END calculateActual()
-
-
-
-    
+        print("The actual value of half life (using decay consant): ", actual_time , " mins")
+    #END calculateActual()
 
 #END CLASS 
--- Checkpoint 2/src/TestingRAD.py
+++ Checkpoint 2/src/TestingRAD.py
@@ -1,23 +1,20 @@
-from RadioactiveDecay import *
+from RadioactiveDecay import RadDecay
 
 # This will ask the user to input the values required and then call the class to start
 # the simulation. 
 def main():
 
-  decay_Const = float(input("Please enter the value for the decay Constant (λ): "))
-  array_Size = int(input("Please enter the size of the array (N): "))
-  time_Stamp = float(input("Please enther the timestamp (Δt): "))
+    decay_Const = float(input("Please enter the value for the decay Constant (λ): "))
+    array_Size = int(input("Please enter the size of the array (N): "))
+    time_Stamp = float(input("Please enther the timestamp (Δt): "))
 
-  rad1 = RadDecay(decay_Const, array_Size, time_Stamp)
-  RadDecay.calculateDecay(rad1)
-  RadDecay.calculateActual(rad1)
-  #rad3 = RadDecay.calculateActual(rad1)
-  #RadDecay.display(rad1, rad2, rad3)
-  
+    rad1 = RadDecay(decay_Const, array_Size, time_Stamp)
+    RadDecay.calculateDecay(rad1)
+    RadDecay.calculateActual(rad1)
 
 #END main()
 
 
 #This is used to make the program run automatically without any arguments 
 if __name__ == "__main__":
-  main()
+    main()
